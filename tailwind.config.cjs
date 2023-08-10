16ff9acb6a41db680e52a90b78e817512995fee9
milosz08 <gilgamilosz451@gmail.com>
2023-08-10 23:31:29 +0200

    rewrite tailwind config for more generic (with refer to moonsphere-base)

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,24 +1,36 @@
 /*
  * Copyright (c) 2023 by MILOSZ GILGA <https://miloszgilga.pl>
  * Silesian University of Technology
- * 
- *   File name: combined.scss
- *   Created at: 2023-08-06, 18:55:39
- *   Last updated at: 2023-08-10, 02:45:23
- * 
+ *
+ *   File name: tailwind.config.cjs
+ *   Created at: 2023-08-06, 18:55:40
+ *   Last updated at: 2023-08-10, 02:46:27
+ *
  *   Project name: moonsphere
  *   Module name: moonsphere-web-client
- * 
+ *
  * This project is a part of "MoonSphere" instant messenger system. This is a project
  * completing a engineers degree in computer science at Silesian University of Technology.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
  * file except in compliance with the License. You may obtain a copy of the License at
- * 
+ *
  *   <http://www.apache.org/license/LICENSE-2.0>
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software distributed under
  * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
  * OF ANY KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the license.
  */
+
+'use scrict';
+
+module.exports = {
+  presets: [
+    require('../moonsphere-base/tailwind/_tailwind.config.cjs')({
+      cdnBaseUrl: process.env.CDN_TAILWIND_PATH,
+      loadableModules: ['auth', 'common', 'footer'],
+    }),
+  ],
+  content: ['./src/**/*.{ejs,ts,html}'],
+};
