24579b0da7710e9edec893b657c5485c5aaa6fe3
milosz08 <gilgamilosz451@gmail.com>
2023-08-11 00:57:40 +0200

    Configure husky hook

---
+++ .eslintrc.js
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2023 by MILOSZ GILGA <https://miloszgilga.pl>
+ * Silesian University of Technology
+ *
+ *   File name: .eslintrc.js
+ *   Created at: 2023-08-11, 00:49:36
+ *   Last updated at: 2023-08-11, 00:51:48
+ *
+ *   Project name: moonsphere
+ *   Module name: moonsphere-web-client
+ *
+ * This project is a part of "MoonSphere" instant messenger system. This is a project
+ * completing a engineers degree in computer science at Silesian University of Technology.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at
+ *
+ *   <http://www.apache.org/license/LICENSE-2.0>
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the license.
+ */
+
+'use scrict';
+
+module.exports = {
+  root: true,
+  ignorePatterns: ['projects/**/*'],
+  overrides: [
+    {
+      files: ['*.ts'],
+      extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@angular-eslint/recommended',
+        'plugin:@angular-eslint/template/process-inline-templates',
+      ],
+      rules: {
+        '@angular-eslint/directive-selector': [
+          'error',
+          {
+            type: 'attribute',
+            prefix: 'msph',
+            style: 'camelCase',
+          },
+        ],
+        '@angular-eslint/component-selector': [
+          'error',
+          {
+            type: 'element',
+            prefix: 'msph',
+            style: 'kebab-case',
+          },
+        ],
+        '@angular-eslint/no-host-metadata-property': 'off',
+        '@typescript-eslint/no-non-null-assertion': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/semi': ['error', 'always'],
+      },
+    },
+    {
+      files: ['*.html'],
+      extends: [
+        'plugin:@angular-eslint/template/recommended',
+        'plugin:@angular-eslint/template/accessibility',
+      ],
+      rules: {},
+    },
+  ],
+};
