5933f0327f4d6a3c4068a922e78bb30ae6ffd2ce
milosz08 <gilgamilosz451@gmail.com>
2023-08-27 02:37:52 +0200

    feat/LSP-0008 - separate register multistage form to multiple ng components

---
+++ src/app/modules/auth/components/register-second-stage-form/register-second-stage-form.component.spec.ts
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2023 by MILOSZ GILGA <https://miloszgilga.pl>
+ * Silesian University of Technology
+ *
+ *   File name: register-second-stage-form.component.spec.ts
+ *   Created at: 2023-08-26, 16:03:09
+ *   Last updated at: 2023-08-26, 16:03:38
+ *
+ *   Project name: moonsphere
+ *   Module name: moonsphere-web-client
+ *
+ * This project is a part of "MoonSphere" instant messenger system. This system is a part of
+ * completing an engineers degree in computer science at Silesian University of Technology.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at
+ *
+ *   <http://www.apache.org/license/LICENSE-2.0>
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the license.
+ */
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RegisterSecondStageFormComponent } from './register-second-stage-form.component';
+
+describe('RegisterSecondStageFormComponent', () => {
+  let component: RegisterSecondStageFormComponent;
+  let fixture: ComponentFixture<RegisterSecondStageFormComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [RegisterSecondStageFormComponent],
+    });
+    fixture = TestBed.createComponent(RegisterSecondStageFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
