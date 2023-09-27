80f308964c5b5d738c03eeea47fe9839483c3b88
milosz08 <gilgamilosz451@gmail.com>
2023-09-28 00:43:02 +0200

    move html button component into lazy button wrapper

--- src/app/modules/shared/components/lazy-button-spinner/lazy-button-spinner.component.ts
+++ src/app/modules/shared/components/lazy-button-spinner/lazy-button-spinner.component.ts
@@ -2,7 +2,7 @@
  * Copyright (c) 2023 by MoonSphere Systems
  * Originally developed by Miłosz Gilga <https://miloszgilga.pl>
  */
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'msph-lazy-button-spinner',
@@ -13,4 +13,9 @@ export class LazyButtonSpinnerComponent {
   @Input() color: 'modal' | '' = '';
   @Input() isLoading = false;
   @Input() i18nText = '';
+  @Input() buttonStyle = 'msph-button msph-button--primary';
+  @Input() buttonDisabled = false;
+  @Input() buttonType: 'button' | 'submit' = 'submit';
+
+  @Output() emitClick = new EventEmitter<void>();
 }
