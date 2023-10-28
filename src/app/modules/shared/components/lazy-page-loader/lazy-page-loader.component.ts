510759844933ecae248244e58cae3250e631a5c9
milosz08 <gilgamilosz451@gmail.com>
2023-10-28 15:30:14 +0200

    change page loader

--- src/app/modules/shared/components/lazy-page-loader/lazy-page-loader.component.ts
+++ src/app/modules/shared/components/lazy-page-loader/lazy-page-loader.component.ts
@@ -2,9 +2,9 @@
  * Copyright (c) 2023 by Visphere & Vsph Technologies
  * Originally developed by Miłosz Gilga <https://miloszgilga.pl>
  */
-import { Location } from '@angular/common';
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '~/env/environment';
 import { lazyPageLoaderFade } from '~/shared-mod/animations/lazy-page-loader.animation';
 import { LazyPageLoaderService } from '~/shared-mod/services/lazy-page-loader/lazy-page-loader.service';
 
@@ -17,18 +17,7 @@ export class LazyPageLoaderComponent {
   isVisible$: Observable<boolean> =
     this._lazyPageLoaderService.lazyLoaderIsVisible$;
 
-  satellites: string[] = [
-    'vsph-page-loader__satellite--first',
-    'vsph-page-loader__satellite--second',
-    'vsph-page-loader__satellite--third',
-  ];
+  cdnPath = environment.contentDistributorBaseUrl;
 
-  constructor(
-    private location: Location,
-    private readonly _lazyPageLoaderService: LazyPageLoaderService
-  ) {}
-
-  url(id: string): string {
-    return `url(${this.location.path()}${id})`;
-  }
+  constructor(private readonly _lazyPageLoaderService: LazyPageLoaderService) {}
 }
