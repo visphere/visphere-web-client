40ca657c15868795b4c93a577ebdba1780206573
milosz08 <gilgamilosz451@gmail.com>
2023-11-08 23:57:42 +0100

    remove unused content

--- src/app/modules/auth/pages/auth-change-password-page/auth-change-password-page.component.ts
+++ src/app/modules/auth/pages/auth-change-password-page/auth-change-password-page.component.ts
@@ -3,17 +3,10 @@
  * Originally developed by Miłosz Gilga <https://miloszgilga.pl>
  */
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'vsph-auth-change-password-page',
   templateUrl: './auth-change-password-page.component.html',
   host: { class: 'flex-grow flex flex-col' },
 })
-export class AuthChangePasswordPageComponent {
-  token: string;
-
-  constructor(private readonly _activatedRoute: ActivatedRoute) {
-    this.token = String(this._activatedRoute.snapshot.paramMap.get('token'));
-  }
-}
+export class AuthChangePasswordPageComponent {}
