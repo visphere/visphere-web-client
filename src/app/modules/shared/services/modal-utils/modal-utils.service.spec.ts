106439c10bcd7081345e3d958f6d5d8cb1dbd323
milosz08 <gilgamilosz451@gmail.com>
2023-11-10 21:03:57 +0100

    minor changes

--- src/app/modules/shared/services/modal-utils/modal-utils.service.spec.ts
+++ src/app/modules/shared/services/modal-utils/modal-utils.service.spec.ts
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2023 by Visphere & Vsph Technologies
+ * Originally developed by Miłosz Gilga <https://miloszgilga.pl>
+ */
 import { TestBed } from '@angular/core/testing';
 import { ModalUtilsService } from './modal-utils.service';
 
