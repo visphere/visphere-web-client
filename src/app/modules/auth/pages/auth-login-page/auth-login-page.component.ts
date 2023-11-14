c17c0fe2807a8a9dcf87b35338925fafc7a9be3f
milosz08 <gilgamilosz451@gmail.com>
2023-11-14 01:14:33 +0100

    update auth login page for oauth2 support

--- src/app/modules/auth/pages/auth-login-page/auth-login-page.component.ts
+++ src/app/modules/auth/pages/auth-login-page/auth-login-page.component.ts
@@ -2,16 +2,45 @@
  * Copyright (c) 2023 by Visphere & Vsph Technologies
  * Originally developed by Miłosz Gilga <https://miloszgilga.pl>
  */
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Oauth2LoginService } from '~/auth-mod/services/oauth2-login/oauth2-login.service';
 import { environment } from '~/env/environment';
+import { AbstractReactiveProvider } from '~/shared-mod/utils/abstract-reactive-provider';
 
 @Component({
   selector: 'vsph-auth-login-page',
   templateUrl: './auth-login-page.component.html',
   host: { class: 'flex-grow flex flex-col' },
+  providers: [Oauth2LoginService],
 })
-export class AuthLoginPageComponent {
+export class AuthLoginPageComponent
+  extends AbstractReactiveProvider
+  implements OnInit, OnDestroy
+{
   path = environment.contentDistributorBaseUrl;
   qrCode =
     'https://upload.wikimedia.org/wikipedia/commons/5/5e/QR_Code_example.png';
+
+  constructor(
+    private readonly _route: ActivatedRoute,
+    private readonly _router: Router,
+    private readonly _oauthLoginService: Oauth2LoginService
+  ) {
+    super();
+    this._oauthLoginService.parseQueryParamArgs(this._route);
+  }
+
+  ngOnInit(): void {
+    this.wrapAsObservable(
+      this._oauthLoginService.performLoginViaProvider$()
+    ).subscribe({
+      next: async () => this._router.navigateByUrl('/'),
+      error: async () => this._router.navigateByUrl('/auth/login'),
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.unmountAllSubscriptions();
+  }
 }
