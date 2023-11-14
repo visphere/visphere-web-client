93f226b8f9b372d0096fdd5ce3cec095616e7608
milosz08 <gilgamilosz451@gmail.com>
2023-11-14 01:12:10 +0100

    update oauth2 button component for auto-login

--- src/app/modules/auth/components/oauth2-button/oauth2-button.component.ts
+++ src/app/modules/auth/components/oauth2-button/oauth2-button.component.ts
@@ -2,16 +2,69 @@
  * Copyright (c) 2023 by Visphere & Vsph Technologies
  * Originally developed by Miłosz Gilga <https://miloszgilga.pl>
  */
-import { Component, Input } from '@angular/core';
-import { Oauth2Type } from '~/auth-mod/types/oauth2.type';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, Input, OnDestroy, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Oauth2LoginService } from '~/auth-mod/services/oauth2-login/oauth2-login.service';
+import { Oauth2Type, oauth2Style } from '~/auth-mod/types/oauth2.type';
 import { environment } from '~/env/environment';
+import { LanguageSwitcherService } from '~/shared-mod/services/language-switcher/language-switcher.service';
+import { OAuth2Supplier } from '~/shared-mod/types/oauth2-supplier.type';
+import { AbstractReactiveProvider } from '~/shared-mod/utils/abstract-reactive-provider';
 
 @Component({
   selector: 'vsph-oauth2-button',
   templateUrl: './oauth2-button.component.html',
-  host: { class: 'flex' },
 })
-export class Oauth2ButtonComponent {
+export class Oauth2ButtonComponent
+  extends AbstractReactiveProvider
+  implements OnInit, OnDestroy
+{
   @Input() oauth2Type!: Oauth2Type;
+
   path = environment.contentDistributorBaseUrl;
+  styles = oauth2Style;
+  lang = '';
+
+  isLoading$: Observable<boolean> = this._oauthLoginService.isLoading$;
+  activeSupplier$: Observable<OAuth2Supplier | null> =
+    this._oauthLoginService.activeSupplier$;
+
+  constructor(
+    private readonly _oauthLoginService: Oauth2LoginService,
+    @Inject(DOCUMENT) private readonly _document: Document,
+    private readonly _languageSwitcherService: LanguageSwitcherService
+  ) {
+    super();
+  }
+
+  ngOnInit(): void {
+    this.wrapAsObservable(
+      this._languageSwitcherService.selectedLang$
+    ).subscribe(({ lang }) => {
+      this.lang = lang;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.unmountAllSubscriptions();
+  }
+
+  handlePerformRedirectToProvider(): void {
+    const params = new URLSearchParams();
+    params.append('be', this._document.location.origin);
+    params.append('le', 'auth/login');
+    params.append('se', 'auth/fill-data');
+    params.append('l', this.lang);
+
+    const baseRedirectUri =
+      environment.infraApiGatewayUrl +
+      '/oauth2/authorization/' +
+      this.oauth2Type;
+
+    const urlWithParams = new URL(baseRedirectUri);
+    urlWithParams.search = params.toString();
+
+    this._document.location.href = urlWithParams.href;
+  }
 }
--- src/app/modules/auth/types/oauth2.type.ts
+++ src/app/modules/auth/types/oauth2.type.ts
@@ -4,3 +4,25 @@
  */
 
 export type Oauth2Type = 'google' | 'facebook' | 'x';
+
+type OauthStyleType = {
+  [key in Oauth2Type]: {
+    btn: string;
+    spn: string;
+  };
+};
+
+export const oauth2Style: OauthStyleType = {
+  google: {
+    btn: 'vsph-auth__google-oauth-button',
+    spn: 'vsph-button-spinner__carousel--google',
+  },
+  facebook: {
+    btn: 'vsph-auth__facebook-oauth-button',
+    spn: 'vsph-button-spinner__carousel--facebook',
+  },
+  x: {
+    btn: 'vsph-auth__x-oauth-button',
+    spn: 'vsph-button-spinner__carousel--x',
+  },
+};
