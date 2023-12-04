5378230ae6bb472f74cf4aa2c7f4c11cf2d19fc0
milosz08 <gilgamilosz451@gmail.com>
2023-12-04 06:46:41 +0100

    remove unused content

--- src/app/modules/client/pages/client-entry-point-page/client-entry-point-page.component.ts
+++ src/app/modules/client/pages/client-entry-point-page/client-entry-point-page.component.ts
@@ -2,33 +2,23 @@
  * Copyright (c) 2023 by Visphere & Vsph Technologies
  * Originally developed by Miłosz Gilga <https://miloszgilga.pl>
  */
-import { Component, OnDestroy } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { FaviconBadgeNotificatorService } from '~/client-mod/services/favicon-badge-notificator/favicon-badge-notificator.service';
 import * as NgrxAction_SHA from '~/shared-mod/store/actions';
 import { SharedReducer } from '~/shared-mod/types/ngrx-store.type';
-import { AbstractReactiveProvider } from '~/shared-mod/utils/abstract-reactive-provider';
 
 @Component({
   selector: 'vsph-client-entrypoint-page',
   templateUrl: './client-entry-point-page.component.html',
 })
-export class ClientEntryPointPageComponent
-  extends AbstractReactiveProvider
-  implements OnDestroy
-{
+export class ClientEntryPointPageComponent {
   constructor(
     private readonly _faviconBadgeNotificationService: FaviconBadgeNotificatorService,
     private readonly _router: Router,
     private readonly _store: Store<SharedReducer>
-  ) {
-    super();
-  }
-
-  ngOnDestroy(): void {
-    this.unmountAllSubscriptions();
-  }
+  ) {}
 
   handleShowNotif(): void {
     this._faviconBadgeNotificationService.showNotify();
