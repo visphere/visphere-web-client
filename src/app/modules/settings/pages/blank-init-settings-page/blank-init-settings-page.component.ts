9284a56a4181e55617c34b2284c0b23be06ce0bd
milosz08 <gilgamilosz451@gmail.com>
2023-12-06 17:10:33 +0100

    add root settings page content

--- src/app/modules/settings/pages/blank-init-settings-page/blank-init-settings-page.component.ts
+++ src/app/modules/settings/pages/blank-init-settings-page/blank-init-settings-page.component.ts
@@ -2,10 +2,37 @@
  * Copyright (c) 2023 by Visphere & Vsph Technologies
  * Originally developed by Miłosz Gilga <https://miloszgilga.pl>
  */
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { environment } from '~/env/environment';
+import { LanguageSwitcherService } from '~/shared-mod/services/language-switcher/language-switcher.service';
+import { AbstractReactiveProvider } from '~/shared-mod/utils/abstract-reactive-provider';
 
 @Component({
   selector: 'vsph-blank-init-settings-page',
   templateUrl: './blank-init-settings-page.component.html',
 })
-export class BlankInitSettingsPageComponent {}
+export class BlankInitSettingsPageComponent
+  extends AbstractReactiveProvider
+  implements OnInit, OnDestroy
+{
+  path = environment.contentDistributorBaseUrl;
+  landingPagePath = environment.baseLandingUrl;
+
+  constructor(
+    private readonly _languageSwitcherService: LanguageSwitcherService
+  ) {
+    super();
+  }
+
+  ngOnInit(): void {
+    this.wrapAsObservable$(
+      this._languageSwitcherService.selectedLang$
+    ).subscribe(lang => {
+      this.landingPagePath = `${environment.baseLandingUrl}${lang.landingPrefix}`;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.unmountAllSubscriptions();
+  }
+}
