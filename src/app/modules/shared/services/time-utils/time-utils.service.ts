480029e5bac3bc043397ad2dcdebcb6f5655f291
milosz08 <gilgamilosz451@gmail.com>
2023-12-09 06:58:29 +0100

    remove format helper and move methods to time utils

--- src/app/modules/shared/services/time-utils/time-utils.service.ts
+++ src/app/modules/shared/services/time-utils/time-utils.service.ts
@@ -6,9 +6,21 @@ import { Injectable } from '@angular/core';
 import * as moment from 'moment';
 import { DateComponentsType } from '~/shared-mod/types/date-components.type';
 import { SpinnerListElementType } from '~/shared-mod/types/spinner-list-element.type';
+import { regex } from '~/shared-mod/validators/regex.constant';
+import { LanguageSwitcherService } from '../language-switcher/language-switcher.service';
 
 @Injectable({ providedIn: 'root' })
 export class TimeUtilsService {
+  private _currentLang = '';
+
+  constructor(
+    private readonly _languageSwitcherService: LanguageSwitcherService
+  ) {
+    this._languageSwitcherService.selectedLang$.subscribe(
+      ({ lang }) => (this._currentLang = lang)
+    );
+  }
+
   generateMonthsForDifferentLocale(lang: string): SpinnerListElementType[] {
     moment.locale(lang);
     return moment.months().map((value, idx) => ({ id: idx + 1, value }));
@@ -40,5 +52,40 @@ export class TimeUtilsService {
       userBirthdate.isValid() &&
       userBirthdate.add(13, 'years').isBefore(moment())
     );
+  }
+
+  formatDateToLocaleShortForm(dateStr: string): string {
+    if (!regex.DATE_VALID.test(dateStr)) {
+      return '';
+    }
+    const splittedDate = dateStr.split('-');
+    if (splittedDate.length !== 3 || !this._currentLang) {
+      return '';
+    }
+    const [year, month, day] = splittedDate;
+    const date = new Date(Number(year), Number(month) - 1, Number(day));
+    return date.toLocaleDateString(this._currentLang, {
+      year: 'numeric',
+      month: 'short',
+      day: '2-digit',
+    });
+  }
+
+  revertStandardDateToSlash(dateStr: string): string {
+    const date = moment(new Date(dateStr));
+    return date.format('DD/MM/YYYY');
+  }
+
+  decomposeDate(dateStr?: string): number[] {
+    if (dateStr) {
+      const date = moment(new Date(dateStr));
+      return [date.daysInMonth(), date.month(), date.year()];
+    }
+    return [1, 1, 1970];
+  }
+
+  composeToDate({ day, month, year }: DateComponentsType): string {
+    const date = moment(new Date(year, month - 1, day));
+    return date.format('DD/MM/YYYY');
   }
 }
