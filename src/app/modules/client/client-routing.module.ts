839dc755060b7fe7dde1b2331edf2db0a4a18214
milosz08 <gilgamilosz451@gmail.com>
2023-12-19 04:57:57 +0100

    append created components to ng module

--- src/app/modules/client/client-routing.module.ts
+++ src/app/modules/client/client-routing.module.ts
@@ -5,7 +5,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ClientRootComponent } from '~/client-mod/client-root.component';
+import { AppEntryPointPageComponent } from '~/client-mod/pages/app-entry-point-page/app-entry-point-page.component';
 import { ClientEntryPointPageComponent } from '~/client-mod/pages/client-entry-point-page/client-entry-point-page.component';
+import { SphereGuildEntryPageComponent } from '~/client-mod/pages/sphere-guild-entry-page/sphere-guild-entry-page.component';
+import { SphereGuildPageComponent } from '~/client-mod/pages/sphere-guild-page/sphere-guild-page.component';
+import { SphereTextChannelPageComponent } from '~/client-mod/pages/sphere-text-channel-page/sphere-text-channel-page.component';
 import { canActivateLoggedRoute } from '../shared/guard/logged-route/logged-route.guard';
 
 const routes: Routes = [
@@ -22,6 +26,30 @@ const routes: Routes = [
             path: '',
             component: ClientEntryPointPageComponent,
             title: 'clientEntryPointPage',
+            children: [
+              {
+                path: '',
+                component: AppEntryPointPageComponent,
+                title: 'clientEntryPointPage',
+              },
+              {
+                path: 'guild/:guildId',
+                component: SphereGuildPageComponent,
+                title: 'clientSphereGuildPage',
+                children: [
+                  {
+                    path: '',
+                    component: SphereGuildEntryPageComponent,
+                    title: 'clientSphereGuildPage',
+                  },
+                  {
+                    path: 'channel/:textChannelId',
+                    component: SphereTextChannelPageComponent,
+                    title: 'clientSphereTextChannelPage',
+                  },
+                ],
+              },
+            ],
           },
         ],
       },
--- src/app/modules/client/client.module.ts
+++ src/app/modules/client/client.module.ts
@@ -4,21 +4,86 @@
  */
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import * as BtsIcon from '@ng-icons/bootstrap-icons';
+import { NgIconsModule } from '@ng-icons/core';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
 import { TranslateModule } from '@ngx-translate/core';
+import { NgxFloatUiModule } from 'ngx-float-ui';
+import { NgxTippyModule } from 'ngx-tippy-wrapper';
 import { ClientRootComponent } from '~/client-mod/client-root.component';
 import { ClientRoutingModule } from '~/client-mod/client-routing.module';
+import { CreateOrJoinSphereModalComponent } from '~/client-mod/components/create-or-join-sphere-modal/create-or-join-sphere-modal.component';
+import { CreateSphereFormComponent } from '~/client-mod/components/create-sphere-form/create-sphere-form.component';
+import { CreateTextChannelModalComponent } from '~/client-mod/components/create-text-channel-modal/create-text-channel-modal.component';
+import { DevastateMemberActionModalComponent } from '~/client-mod/components/devastate-member-action-modal/devastate-member-action-modal.component';
+import { JoinSphereFormComponent } from '~/client-mod/components/join-sphere-form/join-sphere-form.component';
+import { LeftNavigationBarComponent } from '~/client-mod/components/left-navigation-bar/left-navigation-bar.component';
+import { LoggedUserPopupComponent } from '~/client-mod/components/logged-user-popup/logged-user-popup.component';
+import { SphereGuildParticipantComponent } from '~/client-mod/components/sphere-guild-participant/sphere-guild-participant.component';
+import { SphereGuildParticipantsPanelComponent } from '~/client-mod/components/sphere-guild-participants-panel/sphere-guild-participants-panel.component';
+import { SphereTextChannelsPanelComponent } from '~/client-mod/components/sphere-text-channels-panel/sphere-text-channels-panel.component';
+import { AppEntryPointPageComponent } from '~/client-mod/pages/app-entry-point-page/app-entry-point-page.component';
 import { ClientEntryPointPageComponent } from '~/client-mod/pages/client-entry-point-page/client-entry-point-page.component';
+import { SphereGuildEntryPageComponent } from '~/client-mod/pages/sphere-guild-entry-page/sphere-guild-entry-page.component';
+import { SphereGuildPageComponent } from '~/client-mod/pages/sphere-guild-page/sphere-guild-page.component';
+import { SphereTextChannelPageComponent } from '~/client-mod/pages/sphere-text-channel-page/sphere-text-channel-page.component';
+import { SharedModule } from '../shared/shared.module';
+import { clientReduxStore } from './store/reducer';
 
 @NgModule({
-  declarations: [ClientEntryPointPageComponent, ClientRootComponent],
+  declarations: [
+    AppEntryPointPageComponent,
+    ClientEntryPointPageComponent,
+    ClientRootComponent,
+    CreateOrJoinSphereModalComponent,
+    CreateSphereFormComponent,
+    CreateTextChannelModalComponent,
+    DevastateMemberActionModalComponent,
+    JoinSphereFormComponent,
+    LeftNavigationBarComponent,
+    LoggedUserPopupComponent,
+    SphereGuildEntryPageComponent,
+    SphereGuildPageComponent,
+    SphereGuildParticipantComponent,
+    SphereGuildParticipantsPanelComponent,
+    SphereTextChannelPageComponent,
+    SphereTextChannelsPanelComponent,
+  ],
   imports: [
     CommonModule,
-    RouterModule,
     ClientRoutingModule,
-    TranslateModule,
+    NgxFloatUiModule.forRoot({
+      hideOnClickOutside: true,
+      applyClass: 'absolute,z-50',
+    }),
+    EffectsModule.forFeature([]),
     FormsModule,
+    NgIconsModule.withIcons({
+      addIcon: BtsIcon.bootstrapPlus,
+      exploreIcon: BtsIcon.bootstrapCompass,
+      gamingCategory: BtsIcon.bootstrapController,
+      entertainmentCategory: BtsIcon.bootstrapBalloon,
+      techAndScienceCategory: BtsIcon.bootstrapCpu,
+      musicCategory: BtsIcon.bootstrapMusicNoteList,
+      sportsAndTravelCategory: BtsIcon.bootstrapAirplane,
+      otherCategory: BtsIcon.bootstrapQuestion,
+      participantsIcon: BtsIcon.bootstrapPeople,
+      textChannelIcon: BtsIcon.bootstrapChatDots,
+      appendFileIcon: BtsIcon.bootstrapPlusCircle,
+      sendMessageIcon: BtsIcon.bootstrapSend,
+    }),
+    NgxTippyModule,
+    ReactiveFormsModule,
+    RouterModule,
+    SharedModule,
+    StoreModule.forFeature(
+      clientReduxStore.reducerName,
+      clientReduxStore.reducerFunction
+    ),
+    TranslateModule,
   ],
 })
 export class ClientModule {}
