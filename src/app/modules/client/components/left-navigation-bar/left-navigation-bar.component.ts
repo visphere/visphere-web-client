687706f5b88fe00a3f9fcfc17a0252aaafe3167a
milosz08 <gilgamilosz451@gmail.com>
2023-12-19 02:57:45 +0100

    create left navigation bar with logged user popover components

---
+++ src/app/modules/client/components/left-navigation-bar/left-navigation-bar.component.ts
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) 2023 by Visphere & Vsph Technologies
+ * Originally developed by Miłosz Gilga <https://miloszgilga.pl>
+ */
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { NgxTippyProps } from 'ngx-tippy-wrapper';
+import { UserGuildResDto } from '~/client-mod/model/guild.model';
+import { GuildService } from '~/client-mod/services/guild/guild.service';
+import * as NgrxAction_CLN from '~/client-mod/store/actions';
+import { ClientReducer } from '~/client-mod/types/ngx-store.type';
+import { environment } from '~/env/environment';
+import { AbstractLandingUrlProvider } from '~/shared-mod/components/abstract-landing-url-provider';
+import { LoggedUser } from '~/shared-mod/models/logged-user.model';
+import { LanguageSwitcherService } from '~/shared-mod/services/language-switcher/language-switcher.service';
+import { ThemeSwitcherService } from '~/shared-mod/services/theme-switcher/theme-switcher.service';
+import * as NgrxSelector_SHA from '~/shared-mod/store/selectors';
+import { SharedReducer } from '~/shared-mod/types/ngrx-store.type';
+import { floatUiConfig } from '~/shared-mod/utils/float-ui.config';
+
+@Component({
+  selector: 'vsph-left-navigation-bar',
+  templateUrl: './left-navigation-bar.component.html',
+})
+export class LeftNavigationBarComponent
+  extends AbstractLandingUrlProvider
+  implements OnInit, OnDestroy
+{
+  loggedUser?: LoggedUser;
+  userGuilds: UserGuildResDto[] = [];
+
+  selectedLang$ = this._languageSwitcherService.selectedLang$;
+
+  readonly path = environment.contentDistributorBaseUrl;
+  readonly defaultPrefix = 'vsph.clientCommon.client';
+  readonly floatUiStyles = floatUiConfig.floatUiStyles;
+  readonly tooltipProps: NgxTippyProps = {
+    placement: 'right',
+    theme: 'vsph-auth',
+    animation: 'scale-subtle',
+  };
+
+  constructor(
+    private readonly _store: Store<SharedReducer | ClientReducer>,
+    private readonly _languageSwitcherService: LanguageSwitcherService,
+    private readonly _guildService: GuildService,
+    private readonly _router: Router,
+    _themeSwitcherService: ThemeSwitcherService
+  ) {
+    super(_themeSwitcherService, _languageSwitcherService);
+  }
+
+  ngOnInit(): void {
+    this.loadLandingUrl();
+    this.loadBrandThemedIcon();
+    this.wrapAsObservable$(
+      this._store.select(NgrxSelector_SHA.selectLoggedUser)
+    ).subscribe(loggedUser => (this.loggedUser = loggedUser ?? undefined));
+    this.wrapAsObservable$(this._guildService.getAllUserGuilds$()).subscribe(
+      guilds => (this.userGuilds = guilds)
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.unmountAllSubscriptions();
+  }
+
+  handleOpenAddSphereModal(): void {
+    this._store.dispatch(
+      NgrxAction_CLN.__openSelectedModal({ modal: 'add-sphere' })
+    );
+  }
+
+  createGuildRoute(guild: UserGuildResDto): string {
+    return `/guild/${guild.id}`;
+  }
+
+  isActiveRoute(path: string, isExact: boolean): boolean {
+    return this._router.isActive(path, {
+      paths: isExact ? 'exact' : 'subset',
+      queryParams: 'exact',
+      fragment: 'ignored',
+      matrixParams: 'ignored',
+    });
+  }
+}
---
+++ src/app/modules/client/components/logged-user-popup/logged-user-popup.component.ts
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2023 by Visphere & Vsph Technologies
+ * Originally developed by Miłosz Gilga <https://miloszgilga.pl>
+ */
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { AbstractIconThemeProvider } from '~/shared-mod/components/abstract-icon-theme-provider';
+import { LoggedUser } from '~/shared-mod/models/logged-user.model';
+import { LanguageSwitcherService } from '~/shared-mod/services/language-switcher/language-switcher.service';
+import { ThemeSwitcherService } from '~/shared-mod/services/theme-switcher/theme-switcher.service';
+import * as NgrxAction_SHA from '~/shared-mod/store/actions';
+import * as NgrxSelector_SHA from '~/shared-mod/store/selectors';
+import { SharedReducer } from '~/shared-mod/types/ngrx-store.type';
+
+@Component({
+  selector: 'vsph-logged-user-popup',
+  templateUrl: './logged-user-popup.component.html',
+})
+export class LoggedUserPopupComponent
+  extends AbstractIconThemeProvider
+  implements OnInit, OnDestroy
+{
+  loggedUser?: LoggedUser;
+
+  selectedLang$ = this._languageSwitcherService.selectedLang$;
+
+  readonly defaultPrefix = 'vsph.clientCommon.client';
+
+  constructor(
+    private readonly _store: Store<SharedReducer>,
+    private readonly _languageSwitcherService: LanguageSwitcherService,
+    private readonly _router: Router,
+    _themeSwitcherService: ThemeSwitcherService
+  ) {
+    super(_themeSwitcherService);
+  }
+
+  ngOnInit(): void {
+    this.loadBrandThemedIcon();
+    this.wrapAsObservable$(
+      this._store.select(NgrxSelector_SHA.selectLoggedUser)
+    ).subscribe(loggedUser => (this.loggedUser = loggedUser ?? undefined));
+  }
+
+  ngOnDestroy(): void {
+    this.unmountAllSubscriptions();
+  }
+
+  async handleGotoSettings(): Promise<void> {
+    this._store.dispatch(
+      NgrxAction_SHA.__setSettingsReturnUrl({ url: this._router.url })
+    );
+    await this._router.navigateByUrl('settings');
+  }
+
+  handleOpenLogoutModal(): void {
+    this._store.dispatch(
+      NgrxAction_SHA.__updateLogoutModalState({ isOpen: true })
+    );
+  }
+}
