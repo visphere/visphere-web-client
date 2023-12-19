8887185680f02eed96f7f7a33e49f056f33e5226
milosz08 <gilgamilosz451@gmail.com>
2023-12-19 05:00:54 +0100

    fix: inject ElementRef

--- src/app/modules/shared/directives/auto-resizable-text-area/auto-resizable-text-area.directive.spec.ts
+++ src/app/modules/shared/directives/auto-resizable-text-area/auto-resizable-text-area.directive.spec.ts
@@ -1,8 +1,14 @@
+/*
+ * Copyright (c) 2023 by Visphere & Vsph Technologies
+ * Originally developed by Miłosz Gilga <https://miloszgilga.pl>
+ */
+import { ElementRef, inject } from '@angular/core';
 import { AutoResizableTextAreaDirective } from './auto-resizable-text-area.directive';
 
 describe('AutoResizableTextAreaDirective', () => {
   it('should create an instance', () => {
-    const directive = new AutoResizableTextAreaDirective();
+    const elementRef = inject(ElementRef);
+    const directive = new AutoResizableTextAreaDirective(elementRef);
     expect(directive).toBeTruthy();
   });
 });
