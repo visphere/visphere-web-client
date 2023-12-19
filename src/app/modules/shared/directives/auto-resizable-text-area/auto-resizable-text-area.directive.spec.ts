0aac22a28087a5be2c708c44a7a2b8c9e12c7171
milosz08 <gilgamilosz451@gmail.com>
2023-12-19 05:10:08 +0100

    fix unit test injection: add ElementRef mock

--- src/app/modules/shared/directives/auto-resizable-text-area/auto-resizable-text-area.directive.spec.ts
+++ src/app/modules/shared/directives/auto-resizable-text-area/auto-resizable-text-area.directive.spec.ts
@@ -2,11 +2,23 @@
  * Copyright (c) 2023 by Visphere & Vsph Technologies
  * Originally developed by Miłosz Gilga <https://miloszgilga.pl>
  */
-import { ElementRef, inject } from '@angular/core';
+import { ElementRef } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
 import { AutoResizableTextAreaDirective } from './auto-resizable-text-area.directive';
 
+export class MockElementRef extends ElementRef {
+  override nativeElement = {};
+}
+
 describe('AutoResizableTextAreaDirective', () => {
-  const elementRef = inject(ElementRef);
+  let elementRef: ElementRef;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [{ provide: ElementRef, useClass: MockElementRef }],
+    });
+    elementRef = TestBed.inject(ElementRef);
+  });
 
   it('should create an instance', () => {
     const directive = new AutoResizableTextAreaDirective(elementRef);
